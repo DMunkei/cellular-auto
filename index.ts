9d4a521da644b46fef957dd0e7dc086b67346165
DMunkei <stareye863@gmail.com>
2024-03-19 11:16:20 +0100

    feat(ui): add more buttons

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
+
+function getElementOrError<T>(id:string): T{
+  let element = document.getElementById(`${id}`) as T;
+  if (element === null){
+    throw new Error(`Can't find element ${element}.`);
+  }
+  return element;
+}
+
+
 function drawCheckedBoard(ctx: CanvasRenderingContext2D) {
   ctx.strokeStyle = "#000000";
   for (let i = 0; i < CELL_HEIGHT + 1; ++i) {
@@ -14,47 +24,63 @@ function drawCheckedBoard(ctx: CanvasRenderingContext2D) {
   }
 }
 
+const ALIVE = 1;
+const DEAD = 0;
 const width = 800;
 const height = 800;
 const BOARD_ROW = 18;
 const BOARD_COL = BOARD_ROW;
 
-interface Cell {
-  state: number;
-}
+type Cell = number;
 
 class Board {
   rows: number = 0;
-  cols: number = 0;
+  columns = 0;
   cells: Cell[][] = [];
-  constructor(rows: number, cols: number) {
+  toroidalBoard: boolean = false;
+
+  constructor(rows: number, cols: number, toroidal = false) {
     this.rows = rows;
-    this.cols = cols;
+    this.columns=cols;
+    this.toroidalBoard = toroidal;
     this.generateBoard();
   }
 
   generateBoard() {
     for (let r = 0; r < this.rows; ++r) {
       let row: Cell[] = [];
-      for (let c = 0; c < this.cols; ++c) {
-        row.push({ state: 0 });
+      for (let c = 0; c < this.columns; ++c) {
+        row.push(0);
       }
       this.cells.push(row);
     }
   }
 
-  getCellState(row: number, col: number) {
-    return this.cells[row][col].state !== undefined
-      ? this.cells[row][col].state
-      : 0;
+  getCellState(row: number, col: number):number {
+    if (this.toroidalBoard){
+      row = this.posmod(row, this.rows);
+      col = this.posmod(col, this.columns);
+
+    }else {
+      if (!this.inBounds(row,col)) return DEAD;
+    }
+    return this.cells[row][col] == 1? 1:0;
+  }
+  
+  posmod(n:number, m:number){
+    return (n%m+m)%m
+  }
+
+  wrapAround(cell:number){
+    return cell%this.rows == 0? 0: cell;
   }
 
   setCellState(row: number, col: number, state: number) {
-    this.cells[row][col].state = state;
+    this.cells[row][col] = state;
   }
 
   inBounds(row: number, col: number) {
-    return row >= 0 && row < this.rows && col >= 0 && col < this.cols;
+    return row >= 0 && row < this.rows && col >= 0 && col < this.columns;
   }
 
   countAliveNeighbours(r0: number, c0: number): number {
@@ -62,15 +88,17 @@ class Board {
     for (let dr = -1; dr <= 1; ++dr)
       for (let dc = -1; dc <= 1; ++dc) {
         if (dr == 0 && dc == 0) continue;
-        if (!this.inBounds(dr + r0, dc + c0)) continue;
-        result += this.getCellState(r0 + dr, c0 + dc) === 1 ? 1 : 0;
+        const nextX =  r0 + dr;
+        const nextY =  c0 + dc;
+        result += this.getCellState(nextX, nextY) === ALIVE ? ALIVE : DEAD;
       }
     return result;
   }
+
   configureBoard(configuration: number[][]) {
     for (let i = 0; i < configuration.length; ++i) {
       for (let j = 0; j < configuration[0].length; ++j) {
-        this.cells[i][j].state = configuration[i][j];
+        this.cells[i][j] = configuration[i][j];
       }
     }
   }
@@ -78,18 +106,15 @@ class Board {
 
 const fillStates = ["#505050", "#FF5050"];
 
-let app = document.getElementById("app") as HTMLCanvasElement;
-if (app == null) {
-  throw new Error(`Can't find canvas.`);
-}
+let app = getElementOrError<HTMLCanvasElement>('app');
 
 app.width = width;
 app.height = height;
 const CELL_WIDTH = app.width / BOARD_COL;
 const CELL_HEIGHT = app.height / BOARD_ROW;
 
-let currentBoard: Board = new Board(BOARD_ROW, BOARD_COL);
-let nextBoard: Board = new Board(BOARD_ROW, BOARD_COL);
+let currentBoard: Board = new Board(BOARD_ROW, BOARD_COL, true);
+let nextBoard: Board = new Board(BOARD_ROW, BOARD_COL, true);
 
 let ctx = app.getContext("2d") as CanvasRenderingContext2D;
 if (ctx === null) {
@@ -115,17 +140,13 @@ function render(ctx: CanvasRenderingContext2D, board: Board) {
 // Any live cell with more than three live neighbours dies, as if by overpopulation.
 // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 function generateNewBoard(currentBoard: Board, nextBoard: Board) {
-  const DEAD: number = 0;
-  const ALIVE: number = 1;
   for (let row = 0; row < BOARD_ROW; ++row) {
     for (let col = 0; col < BOARD_COL; ++col) {
       const aliveNeighbors = currentBoard.countAliveNeighbours(row, col);
       let currentCellState = currentBoard.getCellState(row, col);
       let nextCellState = currentCellState;
-      if (currentCellState == DEAD && aliveNeighbors == 3)
-        nextCellState = ALIVE; //reproduction
-      if (currentCellState == ALIVE && aliveNeighbors in [2, 3])
-        nextCellState = ALIVE; // next gen
+      if (currentCellState == DEAD && aliveNeighbors == 3) nextCellState = ALIVE; //reproduction
+      if (currentCellState == ALIVE && aliveNeighbors in [2, 3]) nextCellState = ALIVE; // next gen
       if (currentCellState == ALIVE && aliveNeighbors < 2) nextCellState = DEAD; //underpopulation
       if (currentCellState == ALIVE && aliveNeighbors > 3) nextCellState = DEAD; //overpopulation
       nextBoard.setCellState(row, col, nextCellState);
@@ -141,13 +162,14 @@ app.addEventListener("click", (e) => {
   drawCheckedBoard(ctx);
 });
 
-let nextRender = document.getElementById("next") as HTMLButtonElement;
-let play = document.getElementById("play") as HTMLButtonElement;
-let stopbtn = document.getElementById("stop") as HTMLButtonElement;
-let playbackSpeed = document.getElementById(
-  "playbackSpeed",
-) as HTMLInputElement;
-nextRender.addEventListener("click", () => {
+let next = getElementOrError<HTMLButtonElement>("next");
+let play = getElementOrError<HTMLButtonElement>("play");
+
+let drawbtn = getElementOrError<HTMLButtonElement>("draw");
+let stopbtn = getElementOrError<HTMLButtonElement>("stop");
+let playbackSpeed = getElementOrError<HTMLInputElement>("playbackSpeed");
+
+next.addEventListener("click", () => {
   generateNewBoard(currentBoard, nextBoard);
   [currentBoard, nextBoard] = [nextBoard, currentBoard];
   render(ctx, currentBoard);
@@ -157,10 +179,8 @@ nextRender.addEventListener("click", () => {
 let playbackID = -1;
 
 play.addEventListener("click", () => {
-  console.log("doing work");
   if (playbackID > -1) return;
   playbackID = setInterval(() => {
-    console.log(playbackSpeed.valueAsNumber * 500);
     generateNewBoard(currentBoard, nextBoard);
     [currentBoard, nextBoard] = [nextBoard, currentBoard];
     render(ctx, currentBoard);
